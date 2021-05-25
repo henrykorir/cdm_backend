d446f2c9bfad1a41c1ecc76a9f0d15f8e8bfeb47
henrykorir <longkhery@gmail.com>
2021-05-25 22:19:51 +0300

    Added routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,13 @@
 import express from 'express' 
 
-import categoriesReportController from '../controllers/controllers'
+import {
+	categoriesReportController,
+	patientsRecordController
+}from '../controllers/controllers'
 
 const router = express.Router()
 
 router.get('/', categoriesReportController)
+router.get('/patients', patientsRecordController)
 
 export default router
