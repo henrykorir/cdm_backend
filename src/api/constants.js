0f464ce2f443a55df0c7a7761d5aad68e16ebf36
henrykorir <longkhery@gmail.com>
2021-05-25 23:50:15 +0300

    Added query to retrieve cdm category report

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,62 +1,40 @@
 
-/*const cdm_categories_report = `
-				select 
-					table_a.name as 'Location',
-					table_a.NH as 'New Hypertensive',
-					table_a.KH as 'Known Hypertensive',
-					table_a.ND as 'New Diabetic',
-					table_a.KD as 'Known Diabetic',
-					table_a.HD as 'Hypertensive and Diabetic'
-				from(
-						select
-								location.name,
-								flat_cdm_summary.location_id,
-								sum(if(flat_cdm_summary.htn_status = 7285, 1, 0)) as 'NH',
-								sum(if(flat_cdm_summary.htn_status = 7286, 1, 0)) as 'KH',
-								sum(if(flat_cdm_summary.dm_status = 7281, 1, 0)) as 'ND',
-								sum(if(flat_cdm_summary.dm_status = 7282, 1, 0)) as 'KD',
-								sum(
-									if(
-										(	(flat_cdm_summary.htn_status = 7285 and flat_cdm_summary.dm_status = 7281) || 
-											(flat_cdm_summary.htn_status = 7285 and flat_cdm_summary.dm_status = 7282) ||
-											(flat_cdm_summary.htn_status = 7286 and flat_cdm_summary.dm_status = 7281) ||
-											(flat_cdm_summary.htn_status = 7286 and flat_cdm_summary.dm_status = 7282)
-										)
-										, 
-										1, 
-										0
-									)
-								) as 'HD'
-						from flat_cdm_summary inner join location on location.id = flat_cdm_summary.location_id
-						group by flat_cdm_summary.location_id
-				)table_a
-				inner join location on location.id = table_a.location_id;
-`;
-*/
 const cdm_categories_report = `
-	select
-			location.name,
-			flat_cdm_summary.location_id,
-			sum(if(flat_cdm_summary.htn_status = 7285, 1, 0)) as 'NH',
-			sum(if(flat_cdm_summary.htn_status = 7286, 1, 0)) as 'KH',
-			sum(if(flat_cdm_summary.dm_status = 7281, 1, 0)) as 'ND',
-			sum(if(flat_cdm_summary.dm_status = 7282, 1, 0)) as 'KD',
-			sum(
-				if(
-					(	(flat_cdm_summary.htn_status = 7285 and flat_cdm_summary.dm_status = 7281) || 
-						(flat_cdm_summary.htn_status = 7285 and flat_cdm_summary.dm_status = 7282) ||
-						(flat_cdm_summary.htn_status = 7286 and flat_cdm_summary.dm_status = 7281) ||
-						(flat_cdm_summary.htn_status = 7286 and flat_cdm_summary.dm_status = 7282)
-					)
-					, 
-					1, 
-					0
-				)
-			) as 'HD'
-	from flat_cdm_summary 
-	inner join location on location.id = flat_cdm_summary.location_id
-	group by flat_cdm_summary.location_id;
-`
+	select 
+		concat(year(flat_cdm_summary.encounter_datetime),'-',month(flat_cdm_summary.encounter_datetime)) as Month,
+		table_a.name as 'Location',
+		table_a.NH as 'New Hypertensive',
+		table_a.KH as 'Known Hypertensive',
+		table_a.ND as 'New Diabetic',
+		table_a.KD as 'Known Diabetic',
+		table_a.HD as 'Hypertensive and Diabetic'
+	from(
+			select
+					location.name,
+					flat_cdm_summary.location_id,
+					sum(if(flat_cdm_summary.htn_status = 7285, 1, 0)) as 'NH',
+					sum(if(flat_cdm_summary.htn_status = 7286, 1, 0)) as 'KH',
+					sum(if(flat_cdm_summary.dm_status = 7281, 1, 0)) as 'ND',
+					sum(if(flat_cdm_summary.dm_status = 7282, 1, 0)) as 'KD',
+					sum(
+						if(
+							(	(flat_cdm_summary.htn_status = 7285 and flat_cdm_summary.dm_status = 7281) || 
+								(flat_cdm_summary.htn_status = 7285 and flat_cdm_summary.dm_status = 7282) ||
+								(flat_cdm_summary.htn_status = 7286 and flat_cdm_summary.dm_status = 7281) ||
+								(flat_cdm_summary.htn_status = 7286 and flat_cdm_summary.dm_status = 7282)
+							)
+							, 
+							1, 
+							0
+						)
+					) as 'HD'
+			from flat_cdm_summary inner join location on location.id = flat_cdm_summary.location_id
+			group by flat_cdm_summary.location_id
+	)table_a
+	inner join flat_cdm_summary on flat_cdm_summary.location_id = table_a.location_id
+	group by Location, month, table_a.NH, table_a.KH, table_a.ND, table_a.KD, table_a.HD;
+`;
+
 const patients_record_report = `
 	select 
 		patient.name as 'PatientName', 
