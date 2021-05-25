f2f8a4d770056fed67c02cfd162b38fec7e599c8
henrykorir <longkhery@gmail.com>
2021-05-25 10:01:10 +0300

    Added common functions for db queries

---
+++ src/api/functions.js
@@ -0,0 +1,12 @@
+import connection from 'connection' 
+
+export const cdm_categories = () => {
+	connection.connect()
+	
+	connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+		if (err) throw err
+		console.log('The solution is: ', rows[0].solution)
+	})
+	
+	connection.end()
+}
