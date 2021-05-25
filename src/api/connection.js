131ec106bb58d75211b5e918050c9f9724a9fb0f
henrykorir <longkhery@gmail.com>
2021-05-25 07:19:48 +0300

    Added mysql connection api

---
+++ src/api/connection.js
@@ -0,0 +1,10 @@
+import mysql from 'mysql' 
+
+const connection = mysql.createConnection({
+  host: '142.93.103.37',
+  user: 'test_user',
+  password: 'Eek6FEuxS7Y8IGlV@2021',
+  database: 'testDB'
+}
+
+export default connection
